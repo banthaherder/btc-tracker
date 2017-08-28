a78f7c2a1221529341c48faa24413563f09386a8
Aaron Rohrbacher <arjazz@icloud.com>
2017-08-28 11:40:46 -0700

    display last item of a price array at top of page

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 //Busniness logic
-
+//This will eventually return actual bitcoin prices.
+var btcPrices = ["fakePrice1", "fakePrice2", "fakePrice3"];
+function displayLastPrice() {
+  for (i = 0; i<btcPrices.length; ++i) {
+    var index = i
+  }
+  return btcPrices[index];
+}
 //UI Logic
 $(document).ready(function() {
   //This will evenually call a function in the business logic:
-  $("#btc-price").text("THIS WILL BE THE CURRENT BITCOIN PRICE.");
+  $("#btc-price").text(displayLastPrice());
 });
