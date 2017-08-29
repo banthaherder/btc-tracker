0493c1ab10668e48631e4b02a59a7b0f36d4b0e9
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-08-29 09:14:28 -0700

    complete the functionality of the converter

--- js/scripts.js
+++ js/scripts.js
@@ -14,19 +14,11 @@ function Portfolio(name, initialValue) {
   this.name = name;
   this.initialValue = initialValue;
 }
-// var btcPrices = ["fakePrice1", "fakePrice2", "fakePrice3"];
-
-// function displayLastPrice() {
-//   for (i = 0; i<btcPrices.length; ++i) {
-//     var index = i
-//   }
-//   return btcPrices[index];
-// }
 
 //UI Logic
 $(document).ready(function() {
 //Portfolio stuff
-  $("#newPortfolio").submit(function(event){
+  $("#newPortfolio").submit(function(event) {
     event.preventDefault();
     getCurrentBtcData();
     var newPortfolio = new Portfolio ($("#newPortfolioName").val(), currentBtcData.last);
@@ -41,15 +33,36 @@ $(document).ready(function() {
   getCurrentBtcData();
 
   // Refreshes the BTC price data on the page
-  $("refresh-data").click(function(){
+  $("refresh-data").click(function() {
     getCurrentBtcData();
   });
 
-  $("#converter-link").click(function(){
+  $("#converter-link").click(function() {
+    getCurrentBtcData();
     $("#home").hide();
     $("#converter").show();
+
+    $("#convert").click(function() {
+      var usd = $("#usd").val();
+      $("#btc").val((usd/currentBtcData.last).toFixed(8));
+    });
+    $("#swap").click(function() {
+      // Kind of redunant. If anyone has a better way to clear form fields!
+      $("#btc").val("");
+      $("#btc2").val("");
+      $("#usd").val("");
+      $("#usd2").val("");
+
+      $("#price-converter").toggle();
+      $("#price-converter-swapped").toggle();
+
+      $("#convert").click(function() {
+        var btc = $("#btc2").val();
+        $("#usd2").val((currentBtcData.last * btc).toFixed(2));
+      });
+    });
   });
-  $("#home-link").click(function(){
+  $("#home-link").click(function() {
     $("#converter").hide();
     $("#home").show();
   });
